eb8c3015cdc16b6d7608590031b878961dca2a5a
badri-shivani <badrishivani5@gmail.com>
2025-07-01 11:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 # load summarizer
 @st.cache_resource
 def load_summarizer():
-    return pipeline("summarization", model="model="t5-small")
+    return pipeline("summarization", model="t5-small")
 
 summarizer = load_summarizer()
 
